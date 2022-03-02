c4143501999403ec91b68029ca90a95988270091
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-03-02 22:53:55 +0100

    added a component to render twits

--- components/Twit.js
+++ components/Twit.js
@@ -1,8 +1,24 @@
 import styled from "styled-components";
 
 const TwitCard = styled.div`
+  border-radius: 10px;
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  width: 400px;
+  height: 200px;
+  align-items: center;
+  background-color: #9ec3ff;
+  border: 3px solid black;
+  gap: 20px;
+  color: #fff;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: left;
+  justify-content: center;
+  list-style-type: none;
+  box-shadow: 2px 2px 31px 6px rgba(0, 0, 0, 0.48);
 `;
 const Twit = ({ twit: { text, date, likes } }) => {
   return (
